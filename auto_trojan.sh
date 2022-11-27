129135910c84a0880fc422aa6855b5a1b9f556fa
fistnine <fistnine@outlook.com>
2022-11-27 13:53:34 +0800

    优化auto_trojan.sh

--- auto_trojan.sh
+++ auto_trojan.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-## auto_trojan.sh 9 1 #更换到节点9并强制更新
+#适用于docker trojan-go
+#auto_trojan.sh 9 1 #更换到节点9并强制更新
 #传入第一个参数 用来判断切换到第几个节点
 var1=${1} #节点数字
 var2=${2} #是否联网更新节点 默认0为不更新 1为强制更新
 
+_command="sudo docker restart trojan-go" #重启trojan-go的命令
 _file=$auto_trojan_file #输出文件 ~/docker_conf/trojan-go/config.json
 _cache=$_file.cache #缓存文件
 _url=$auto_trojan_url #订阅的节点 "https://******"
 
 function write(){ #写入文件
 	str=${1}
+	_nowpoint=${1##*#} #截取节点的描述
 	#获取密码
 	_password=${str%%@*}
 	_password=${_password#*trojan://}
@@ -40,9 +43,11 @@ function write(){ #写入文件
 	echo "        ]" >> $_file
 	echo "    }" >> $_file
 	echo "}" >> $_file
-	echo "#已切换#"
-	printf $(echo -n $str | sed 's/\\/\\\\/g;s/\(%\)\([0-9a-fA-F][0-9a-fA-F]\)/\\x\2/g')"\n"
-	sudo docker restart trojan-go #重启trojan-go
+	echo "# 已切换置节点:"
+	printf $(echo -n $_nowpoint | sed 's/\\/\\\\/g;s/\(%\)\([0-9a-fA-F][0-9a-fA-F]\)/\\x\2/g')"\n"
+	echo "正在重启服务..."
+	$_command #重启trojan
+	echo "服务已重启..."
 }
 
 function update(){ #获取订阅 解码 写入缓存
@@ -50,7 +55,7 @@ function update(){ #获取订阅 解码 写入缓存
 		#如果存在文件 执行备份
 		#cp $_file $(date "+%Y%m%d%H%M%S")$_file.backup
 	#fi #备份功能未开启
-	echo "正在更新缓存文件... \n $_cache"
+	echo "正在更新缓存文件: $_cache"
 	unset http_proxy #取消使用代理
 	unset https_proxy
 	curl $_url | base64 -d > $_cache
@@ -68,15 +73,20 @@ if [ -f $_cache ] ; then #判断是否存在缓存文件
 else
 	echo "未发现缓存文件 强制更新缓存文件..."
 	update
+	echo "更新完成"
 fi
 
-#获得行数
-line=`wc -l $_cache`
+_nowpoint=`grep remote_addr $_file` #获取当前缓存中的节点文字 用于最后显示当前节点
+_nowpoint=${_nowpoint##*\"remote_addr\": \"}
+_nowpoint=${_nowpoint%%\"*}
+
+line=`wc -l $_cache` #获得缓存的行数
 line=${line%% *}
 
 if [[ -n $var1 ]] ; then #是否有第1个参数
 	#判断传入的数字是否超出整个节点范围
 	if (($var1 <= $line)) ; then
+		#从缓存中获取节点
 		str=`sed -n "$var1"p $_cache` #获取当前行的数据
 		write $str #写入节点
 	else
@@ -85,19 +95,26 @@ if [[ -n $var1 ]] ; then #是否有第1个参数
 	fi
 else
 	#没有传入参数则不切换 列出所有节点
-	echo "#缓存中的所有节点#"
+	echo "-----缓存中的所有节点-----"
 	#获取缓存中的所有的节点名称 并且在前面标注编号
  	for i in `cat $_cache`
  	do
  		line1=$((line1+1))
- 		i=${i##*#}
+		pointdescribe=${i##*#} #截取节点的描述
+ 		istr=$i #获取当前节点的文字 用作判断
+ 		istr=${istr#*@}
+		istr=${istr%%:*}
+		if [ "$_nowpoint" == "$istr" ] ; then
+			_nowpoint=$(printf $(echo -n $pointdescribe | sed 's/\\/\\\\/g;s/\(%\)\([0-9a-fA-F][0-9a-fA-F]\)/\\x\2/g'))
+			_nowpoint="$line1"--"$_nowpoint" #获取当前节点的名字
+		fi
  		printf "$line1--"
- 		printf $(echo -n $i | sed 's/\\/\\\\/g;s/\(%\)\([0-9a-fA-F][0-9a-fA-F]\)/\\x\2/g')"\n"
+ 		#将url字符转换为可读字符
+ 		printf $(echo -n $pointdescribe | sed 's/\\/\\\\/g;s/\(%\)\([0-9a-fA-F][0-9a-fA-F]\)/\\x\2/g')"\n"
  	done
-	echo "#未修改节点#"
+ 	echo "当前节点为:"
+	echo "$_nowpoint"
+	echo "未修改节点"
 fi
-echo "一共有节点 $line"
 
-str=`grep remote_addr $_file`
-str=${str##*: }
-echo "当前节点为$str"
+echo "一共有$line个节点"
